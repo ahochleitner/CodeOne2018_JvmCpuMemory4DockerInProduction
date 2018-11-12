10737186f6310af56fed74c4b05905612b89f476
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-11-12 18:15:23 +0100

    weitere kleinigkeiten

--- Memory/12_runAllMemoryInfo.sh
+++ Memory/12_runAllMemoryInfo.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-#ADDITIONAL_OPTS="-m 1024m"
-#ADDITIONAL_OPTS="--cpus .5 --cpuset-cpus 1"
 
 MAIN=MemoryInfo
 #MAIN=Main
+
+if [ "$#" = "1" -a "$1" = "-usage" ]; then
+  echo "run Java7 ... Java11-Docker with MemoryInfo inside"
+  echo "usage: $0"
+  echo "usage: $0 -setMemory1024m"
+  echo "usage: $0 -setCpus"
+  exit 1
+fi
+
+if [ "$1" = "-setMemory1024m" ]; then
+  ADDITIONAL_OPTS="-m 1024m"
+  echo "run with $ADDITIONAL_OPTS"
+fi
+if [ "$1" = "-setCpus" ]; then
+  ADDITIONAL_OPTS="--cpus .5 --cpuset-cpus 1"
+  echo "run with $ADDITIONAL_OPTS"
+fi
 
 for i in 7 8 9 10 11; do
   echo "====== " openjdk$i " ====="
