1bab40e254e8f5fe80bbc7771d1ad644a117c8d7
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-11-12 15:40:44 +0100

    kl. erweiterung

--- Memory/11_runMemoryInfoLocal.sh
+++ Memory/11_runMemoryInfoLocal.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+if [ "$#" = "1" -a "$1" = "-usage" ]; then
+  echo "run javac and java both with JAVA8_HOME and JAVA10_HOME"
+  echo "usage: $0"
+  echo "usage: $0 -showVmSettings"
+  echo "usage: $0 -showVmSettings -setXmsXmx"
+  exit 1
+fi
+
+if [ "$1" = "-showVmSettings" ]; then
+  ADDITIONAL_PARAMS="-XshowSettings:vm"
+  echo "run with $ADDITIONAL_PARAMS"
+fi
+if [ "$2" = "-setXmsXmx" ]; then
+  ADDITIONAL_PARAMS2="-Xms1024k -Xmx2048k "
+  echo "run with $ADDITIONAL_PARAMS2"
+fi
+
+echo "JAVA8 ........................."
 $JAVA8_HOME/bin/javac MemoryInfo.java 
-$JAVA8_HOME/bin/java MemoryInfo
+$JAVA8_HOME/bin/java $ADDITIONAL_PARAMS $ADDITIONAL_PARAMS2 MemoryInfo
+echo "JAVA10 ........................."
 $JAVA10_HOME/bin/javac MemoryInfo.java 
-$JAVA10_HOME/bin/java MemoryInfo
+$JAVA10_HOME/bin/java $ADDITIONAL_PARAMS $ADDITIONAL_PARAMS2 MemoryInfo
 
--- README.md
+++ README.md
@@ -8,5 +8,8 @@ cd Memory
 
 run local info (MemoryInfo.java) with java8 and java10.
 
-11_runMemoryInfoLocal.sh
+./11_runMemoryInfoLocal.sh
+./11_runMemoryInfoLocal.sh -usage
+./11_runMemoryInfoLocal.sh -showVmSettings
+./11_runMemoryInfoLocal.sh -showVmSettings -setXmsXmx
 
