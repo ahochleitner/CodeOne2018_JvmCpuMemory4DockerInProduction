ab81143abc55bb13fd2c1398587f332f12cabb12
Andreas Hochleitner <hochleitner@lkw-walter.com>
2018-11-12 22:00:03 +0100

    weitere Skripte

--- Memory/12_runAllMemoryInfo.sh
+++ Memory/12_runAllMemoryInfo.sh
@@ -8,10 +8,11 @@ if [ "$#" = "1" -a "$1" = "-usage" ]; then
   echo "usage: $0"
   echo "usage: $0 -setMemory1024m"
   echo "usage: $0 -setCpus"
+  echo "usage: $0 -setCGroupMemoryLimitForHeap"
   exit 1
 fi
 
-if [ "$1" = "-setMemory1024m" ]; then
+if [ "$1" = "-setMemory1024m" -o  "$1" = "-setCGroupMemoryLimitForHeap" ]; then
   ADDITIONAL_OPTS="-m 1024m"
   echo "run with $ADDITIONAL_OPTS"
 fi
@@ -19,9 +20,13 @@ if [ "$1" = "-setCpus" ]; then
   ADDITIONAL_OPTS="--cpus .5 --cpuset-cpus 1"
   echo "run with $ADDITIONAL_OPTS"
 fi
+if [ "$1" = "-setCGroupMemoryLimitForHeap" ]; then
+  RUN_OPTS="-setCGroupMemoryLimitForHeap"
+  echo "run with $ADDITIONAL_OPTS"
+fi
 
 for i in 7 8 9 10 11; do
   echo "====== " openjdk$i " ====="
-  docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp $ADDITIONAL_OPTS openjdk:$i ./runJavaCompileAndRun.sh $MAIN
+  docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp $ADDITIONAL_OPTS openjdk:$i ./runJavaCompileAndRun.sh $MAIN $RUN_OPTS
   sleep 3
 done
--- Memory/runJavaCompileAndRun.sh
+++ Memory/runJavaCompileAndRun.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-#JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
-#JAVA_OPTS="-XX:ParallelGCThreads=1 -XX:CICompilerCount=2"
 
-if [ "$#" != "1" ]; then
+if [ "$#" != "1" -a "$#" != "2" ]; then
   echo "usage: $0 JavaClass"
   echo "          compile and run JavaClass"
+  echo "       $0 JavaClass -setCGroupMemoryLimitForHeap"
+  echo "       $0 JavaClass -setGCThreads"
   exit 1
 fi
+
+if [ "$#" != "1" -a "$2" = "-setCGroupMemoryLimitForHeap" ]; then
+  JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap"
+fi
+if [ "$#" != "1" -a "$2" = "-setGCThreads" ]; then
+  JAVA_OPTS="-XX:ParallelGCThreads=1 -XX:CICompilerCount=2"
+fi
+
 
 javac $1.java
 java $JAVA_OPTS $1
